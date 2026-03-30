7041438cf6fd8cec1808a254be64892aed9a133c
ArthurS-T <arthur.martins1611@gmail.com>
2026-03-30 10:31:37 -0300

    Commit inicial do projeto minhaSegundaAPI

---
+++ src/main/java/com/arthur/MinhaApi.java
@@ -0,0 +1,13 @@
+package com.arthur;    
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinhaApi {
+
+        public static void main(String[] args){
+            SpringApplication.run(MinhaApi.class, args);
+        }
+    
+}
